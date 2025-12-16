e9f629623822cfe9e1faba3ba604adb7aef90418
Khorsed-Alam <khorsedalam0472@gmail.com>
2025-12-16 18:59:18 +0600

    Mean of array

---
+++ 11_Recursion/2_Medium Problem/01_Mean_of_array.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+double findMean( vector<int> arr){
+    int n= arr.size();
+    if(n==1){
+        return arr[n-1];
+    }else{
+         return (
+            (double)(findMean(vector<int>(arr.begin(), arr.begin() + n-1)) * (n-1) + arr[n-1]) / n); 
+    }
+}
+int main(){
+    double mean=0;
+    vector<int>arr={1,2,3,4,5};
+    cout<<findMean(arr)<<endl;
+    return 0;
+}
