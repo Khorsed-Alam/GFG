56b744ce59edc339f8b4c775085807539114f564
Khorsed-Alam <khorsedalam0472@gmail.com>
2026-01-05 00:56:37 +0600

    initiation and access first and last element

---
+++ 00_STL/04 Array/array.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<deque>
+#include<array>
+using namespace std;
+int main(){
+    array<int,5> arr={ 5,6,7,3,6};
+    for(auto val: arr){
+        cout<<val<<" ";
+    }
+    cout<<endl;
+
+    cout<<"First elemnet of array is: "<<arr.front()<<endl;
+    cout<<"Last element of array is : "<<arr.back()<<endl;
+    return 0;
+}
